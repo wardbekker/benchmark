68f552160eef9d63c8ce4fb9b9f9afc6c8ec3c9a
Ward Bekker <ward@wardbekker.com>
2017-03-20 09:52:14 +0100

    Adding some output

--- src/main/scala/org/ward/Benchmark.scala
+++ src/main/scala/org/ward/Benchmark.scala
@@ -1,11 +1,14 @@
 package org.ward
 
 
+import java.io.{BufferedWriter, FileWriter}
+
 import org.apache.spark.SparkContext._
-import org.apache.spark.{SparkConf,SparkContext}
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.broadcast.Broadcast
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
+
 import util.Random
 import java.lang.System.{currentTimeMillis => _time}
 
@@ -15,13 +18,15 @@ import java.lang.System.{currentTimeMillis => _time}
  */
 object Benchmark {
   def main(args: Array[String]){
-    val conf = new SparkConf().setAppName("Benchmark")
+    val conf = new SparkConf().setAppName("Benchmark").set("spark.hadoop.dfs.replication", "1")
     val sc = new SparkContext(conf)
-
 
     sc.hadoopConfiguration.set("mapred.output.compress", "false")
 
     def profile[R](code: => R, t: Long = _time) = (code, _time - t)
+
+    // This is the output file for statistics
+    val statFile = new BufferedWriter(new FileWriter("Benchmark_write.stat"))
 
     val fs = FileSystem.get(new Configuration(true))
 
@@ -36,10 +41,16 @@ object Benchmark {
     // force calculation
     b.count()
 
-    profile {
+    val (junk, timeW) = profile {
       b.saveAsObjectFile("foo")
-      fs.delete(new Path("foo"), true)
     }
+
+    fs.delete(new Path("foo"), true)
+
+    // Write statistics
+    statFile.write("\nMilliseconds for writing: " + timeW)
+    statFile.close()
+
 
   }
 }
